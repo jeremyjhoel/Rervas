4990b77bae51d078a939a5cfe59e9abc2113c399
jeremyjhoel <133717713+jeremyjhoel@users.noreply.github.com>
2023-06-16 16:01:45 -0400

    Update forms.py

--- Reserva/Reservar/forms.py
+++ Reserva/Reservar/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Bus
+from .models import Ruta
 
 class BusForm(forms.ModelForm):
 
     class Meta:
         model = Bus
         fields = ('patente','cantidadAsientos')
+
+class RutaForm(forms.ModelForm):
+
+    class Meta:
+        model = Ruta
+        fields = ('origen', 'destino', 'tiempoEstimado')
